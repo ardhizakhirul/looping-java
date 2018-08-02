0eda1616d01dff73208df5fed43dfa39d0da7b31
ASUS <ASUS@LAPTOP-D8SLB01C>
2018-08-02 08:18:50 +0700

    for while do-while 02

---
+++ src/jobsheet08/contoh02.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jobsheet08;
+
+
+public class contoh02 {
+    public static void main (String[]args){
+        int x;
+        for(x = 1; x <= 10; x++)
+        {
+            System.out.print(x + " Ardhi zakhirul ");
+        }
+        
+        x = 1;
+        while( x <= 10)
+        {
+            System.out.print("Ardhi Zakhirul Qolby"); 
+            x++;
+        }
+    }
+    
+}
