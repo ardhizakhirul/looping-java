61a43fc6ebf7bd4e84b85cfc1fd71ae8115c732e
ASUS <ASUS@LAPTOP-D8SLB01C>
2018-08-13 08:31:49 +0700

    BILANGAN GANJIL GENAP 10 PERTAMA

---
+++ src/jobsheet11/tugas.java
@@ -0,0 +1,12 @@
+package jobsheet11;
+
+import java.util.Locale;
+
+public class tugas {
+    public static void main (String[] args)
+    {
+        String identitas = "Ardhi Zakhirul Qolby / X-RPL3 / 6";
+        System.out.println("identitas :" + identitas);
+    }
+}
+    
