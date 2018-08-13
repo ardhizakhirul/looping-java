e231bf89aacc8b560d05abf3fe820c641c3f8b38
ASUS <ASUS@LAPTOP-D8SLB01C>
2018-08-13 08:34:38 +0700

    coba method length & isEmpty

--- src/jobsheet11/tugas.java
+++ src/jobsheet11/tugas.java
@@ -7,6 +7,17 @@ public class tugas {
     {
         String identitas = "Ardhi Zakhirul Qolby / X-RPL3 / 6";
         System.out.println("identitas :" + identitas);
+    String x = "Operasi";
+        
+        System.out.println("isi variable x : " + x);
+        System.out.println("\""+ x + "\" panjang karakter = "+ x.length());
+        
+        System.out.println("x adalah kosong : " + x.isEmpty());
+        
+        String y = "";
+        System.out.println("isi variable y : "+ y);
+        System.out.println("y adalah kosong : " + y.isEmpty());
     }
+    
 }
     
